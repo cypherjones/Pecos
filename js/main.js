3a3ab2090f0b7e3c7e1e1fe74cfdc039bc738abf
cypherjones <ajjonez@icloud.com>
2017-11-15 00:53:25 -0600

    add the title height script

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ jQuery(document).ready(function($){
 $('#the-event-loop').each(function(){
 		
 		// cache the highest element
-		let heightestBox = 0;
+		let heightestBox = 0,
+		    titleHeight = 0;
 		
 		// find each title and determine the highest
 		$(this).find('.event-container', this).each(function(){
@@ -19,6 +20,19 @@ $('#the-event-loop').each(function(){
 		// heighest title element
 		$('.event-container', this).css({
 			'height': heightestBox,
+		});
+
+		$(this).find('.event-title', this).each(function(){
+			
+			// make the height of all the titles the heighest
+			if($(this).height() > titleHeight) {
+				titleHeight = $(this).height();
+			}
+			
+		});
+
+		$('.event-title', this).css({
+			'height': titleHeight,
 		});
 
 	});
