13d0468e31670d58388c8beb96141ba65d848f8b
cypherjones <ajjonez@icloud.com>
2017-11-15 00:51:12 -0600

    rewrite height script

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 jQuery(document).ready(function($){
 
-	$('#the-event-loop').each(function(){
-
-		let titleHeight = 0,
-		    thisBoxHeight = 0;
-
+$('#the-event-loop').each(function(){
+		
+		// cache the highest element
+		let heightestBox = 0;
+		
+		// find each title and determine the highest
 		$(this).find('.event-container', this).each(function(){
-
-			if($(this).height() > thisBoxHeight) {
-				thisBoxHeight = $(this).height();
+			
+			// make the height of all the titles the heighest
+			if($(this).height() > heightestBox) {
+				heightestBox = $(this).height();
 			}
 			
-			console.log(thisBoxHeight);
-
-			$('.event-container', this).css({
-				'height': thisBoxHeight,
-			});
-
 		});
 
-
+		// make an inline css based on the
+		// heighest title element
+		$('.event-container', this).css({
+			'height': heightestBox,
+		});
 
 	});
 
