b56c10396671c670fc23bce6ff393b8662bd9aef
cypherjones <ajjonez@icloud.com>
2017-11-15 00:03:16 -0600

    adjust js height script

--- js/main.js
+++ js/main.js
@@ -1,25 +1,35 @@
 jQuery(document).ready(function($){
-	$('.event-wrapper').each(function(){
+	// $('.event-wrapper').each(function(){
 			
-			// cache the highest element
-			let heightestBox = 0;
+	// 		// cache the highest element
+	// 		let heightestBox = 0;
 			
-			// find each title and determine the highest
-			$(this).find('.event-box').each(function(){
-			// $(this).find('.event-box .event-title', this).each(function(){
+	// 		// find each title and determine the highest
+	// 		$(this).find('.event-box').each(function(){
+	// 		// $(this).find('.event-box .event-title', this).each(function(){
 				
-				// make the height of all the titles the heighest
-				if($(this).height() > heightestBox) {
-					heightestBox = $(this).height();
-				}
-				console.log(heightestBox);
-			});
+	// 			// make the height of all the titles the heighest
+	// 			if($(this).height() > heightestBox) {
+	// 				heightestBox = $(this).height();
+	// 			}
+	// 			console.log(heightestBox);
+	// 		});
 
-			// make an inline css based on the
-			// heighest title element
-			$('.event-box', this).css({
-				'height': heightestBox,
-			});
+	// 		// make an inline css based on the
+	// 		// heighest title element
+	// 		$('.event-box', this).css({
+	// 			'height': heightestBox,
+	// 		});
 
-		});
+	// 	});
+
+	$('#event').each(function(){
+
+		let boxHeight = 0;
+
+		if($(this).height() > boxHeight) {
+			boxHeight = $(this).height();
+		};
+		console.log(boxHeight);
+	});
 });
