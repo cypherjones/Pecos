746b4f70ce48692ec83c44bfe765f55301e0630f
cypherjones <ajjonez@icloud.com>
2017-11-14 23:33:21 -0600

    add js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.event-wrapper').each(function(){
+		
+		// cache the highest element
+		let heightestBox = 0;
+		
+		// find each title and determine the highest
+		$(this).find('.event-box .event-title', this).each(function(){
+			
+			// make the height of all the titles the heighest
+			if($(this).height() > heightestBox) {
+				heightestBox = $(this).height();
+			}
+			
+		});
+
+		// make an inline css based on the
+		// heighest title element
+		$('.event-title', this).css({
+			'height': heightestBox,
+		});
+
+	});
