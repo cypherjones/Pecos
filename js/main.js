834158455e8c2b0acbd15662ac71997b0ec97b36
cypherjones <ajjonez@icloud.com>
2017-11-14 23:41:14 -0600

    debug js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-$('.event-wrapper').each(function(){
-		
-		// cache the highest element
-		let heightestBox = 0;
-		
-		// find each title and determine the highest
-		$(this).find('.event-box .event-title', this).each(function(){
+jQuery(document).ready(function($){
+	$('.event-wrapper').each(function(){
 			
-			// make the height of all the titles the heighest
-			if($(this).height() > heightestBox) {
-				heightestBox = $(this).height();
-			}
-			console.log(heightestBox);
-		});
+			// cache the highest element
+			let heightestBox = 0;
+			
+			// find each title and determine the highest
+			$(this).find('.event-box .event-title', this).each(function(){
+				
+				// make the height of all the titles the heighest
+				if($(this).height() > heightestBox) {
+					heightestBox = $(this).height();
+				}
+				console.log(heightestBox);
+			});
 
-		// make an inline css based on the
-		// heighest title element
-		$('.event-title', this).css({
-			'height': heightestBox,
-		});
+			// make an inline css based on the
+			// heighest title element
+			$('.event-title', this).css({
+				'height': heightestBox,
+			});
 
-	});
+		});
+});
