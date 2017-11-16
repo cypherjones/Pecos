44b49c3ee67919f4808ff88575822aba927fda50
cypherjones <ajjonez@icloud.com>
2017-11-15 23:56:38 -0600

    just adjust the height of the titles

--- js/main.js
+++ js/main.js
@@ -3,24 +3,24 @@ jQuery(document).ready(function($){
 $('#the-event-loop').each(function(){
 		
 		// cache the highest element
-		let heightestBox = 0,
-		    titleHeight = 0;
+		// let heightestBox = 0,
+		//     titleHeight = 0;
 		
-		// find each title and determine the highest
-		$(this).find('.event-container', this).each(function(){
+		// // find each title and determine the highest
+		// $(this).find('.event-container', this).each(function(){
 			
-			// make the height of all the titles the heighest
-			if($(this).height() > heightestBox) {
-				heightestBox = $(this).height();
-			}
+		// 	// make the height of all the titles the heighest
+		// 	if($(this).height() > heightestBox) {
+		// 		heightestBox = $(this).height();
+		// 	}
 			
-		});
+		// });
 
-		// make an inline css based on the
-		// heighest title element
-		$('.event-container', this).css({
-			'height': heightestBox,
-		});
+		// // make an inline css based on the
+		// // heighest title element
+		// $('.event-container', this).css({
+		// 	'height': heightestBox,
+		// });
 
 		$(this).find('.event-title', this).each(function(){
 			
