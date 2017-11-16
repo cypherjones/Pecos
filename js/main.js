ebf7a384c47ce65c7245476e7edf147d21b3d309
cypherjones <ajjonez@icloud.com>
2017-11-16 00:24:08 -0600

    float the li left and fix h2 to 22

--- js/main.js
+++ js/main.js
@@ -2,25 +2,25 @@ jQuery(document).ready(function($){
 
 $('#the-event-loop').each(function(){
 		
-		// cache the highest element
-		let heightestBox = 0,
-		    titleHeight = 0;
+		// // cache the highest element
+		// let heightestBox = 0,
+		//     titleHeight = 0;
 		
-		// find each title and determine the highest
-		$(this).find('.event-container', this).each(function(){
+		// // find each title and determine the highest
+		// $(this).find('.event-container', this).each(function(){
 			
-			// make the height of all the titles the heighest
-			if($(this).height() > heightestBox) {
-				heightestBox = $(this).height();
-			}
+		// 	// make the height of all the titles the heighest
+		// 	if($(this).height() > heightestBox) {
+		// 		heightestBox = $(this).height();
+		// 	}
 			
-		});
+		// });
 
-		// make an inline css based on the
-		// heighest title element
-		$('.event-container', this).css({
-			'height': heightestBox,
-		});
+		// // make an inline css based on the
+		// // heighest title element
+		// $('.event-container', this).css({
+		// 	'height': heightestBox,
+		// });
 
 		// $(this).find('.event-meta', this).each(function(){
 			
