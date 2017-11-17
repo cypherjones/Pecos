c4278410f9a64d00b7fcabe0cd52790c2d00dbd9
AJ Jones <ajjonez@icloud.com>
2017-11-16 22:43:36 -0600

    rest

--- tribe-events/pro/photo/loop.php
+++ tribe-events/pro/photo/loop.php
@@ -22,16 +22,22 @@ $more = false;
 	<h2>Memberships</h2>
 </div>
 
-<div id="the-event-loop" class="event-loop">
+<div class="tribe-events-loop tribe-clearfix" id="tribe-events-photo-events">
+	<div class="tribe-events-photo-grid-sizer">
+		<!-- .tribe-events-photo-grid-sizer empty element, only used for element sizing -->
+	</div>
+
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php do_action( 'tribe_events_inside_before_loop' ); ?>
 
 		<!-- Event  -->
-		<div id="event" class="event-container">
+		<div id="post-<?php the_ID() ?>" class="<?php tribe_events_event_classes() ?>">
 			<?php tribe_get_template_part( 'pro/photo/single', 'event' ) ?> 
 		</div>
 
 		<?php do_action( 'tribe_events_inside_after_loop' ); ?>
 	<?php endwhile; ?>
-</div>
-
+	<div class="tribe-events-photo-gutter-sizer">
+		<!-- .tribe-events-photo-gutter-sizer empty element, only used for element sizing -->
+	</div>
+</div><!-- .tribe-events-loop -->
