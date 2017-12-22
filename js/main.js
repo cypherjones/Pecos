f022d233762fc756532a0b0fbcf3f398120bb4fe
AJ Jones <ajjonez@icloud.com>
2017-12-22 02:27:16 -0600

    increment

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ jQuery(document).ready(function($){
 
 			var inc = i + 3;
 
-			$('.event-container').slice(i, inc).wrapInner('<div class="row"></div>');
+			
 
 
 			console.log(`${inc} and ${i}`);
@@ -69,7 +69,7 @@ jQuery(document).ready(function($){
 
 		};
 	};
-	
+	$('.event-container').slice(i, inc).wrapInner('<div class="row"></div>');
 
 
 	// create and index and increment through the events
