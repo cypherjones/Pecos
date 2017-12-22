b7da61bfc17c8b99dc22ec111cbe215c747cb47c
AJ Jones <ajjonez@icloud.com>
2017-12-22 02:31:19 -0600

    slice

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ jQuery(document).ready(function($){
 
 			var inc = i + 3;
 
-			
+			$('.event-container').slice(0,3).wrapAll('<div class="row"></div>');
 
 
 			console.log(`${inc} and ${i}`);
@@ -69,7 +69,7 @@ jQuery(document).ready(function($){
 
 		};
 	};
-	$('.event-container').slice(0,3).wrapAll('<div class="row"></div>');
+	
 
 
 	// create and index and increment through the events
